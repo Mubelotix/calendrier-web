d0483fd87cd71159551f23f3d9ae6dca07fd9656
Mubelotix <mubelotix@gmail.com>
2025-09-20 18:16:15 +0200

    Reinit

--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,3 @@
-use yew::prelude::*;
-use yew_template::*;
-
-struct App {
-
-}
-
-impl Component for App {
-    type Message = ();
-    type Properties = ();
-
-    fn create(_ctx: &Context<Self>) -> Self {
-        Self {}
-    }
-
-    fn view(&self, ctx: &Context<Self>) -> Html {
-        let date = "27 vendémiaire 232";
-        template_html!("src/main.html", ...)
-    }
-}
-
 fn main() {
-    yew::Renderer::<App>::new().render();
+    println!("Hello, world!");
 }
