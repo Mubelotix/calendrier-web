89d23e81175806191aee582ac681e37bd4b675f7
Mubelotix <mubelotix@gmail.com>
2025-09-20 23:10:22 +0200

    Add time offest

--- wrapper/src/lib.rs
+++ wrapper/src/lib.rs
@@ -1,5 +1,6 @@
 use wasm_bindgen::prelude::*;
 use calendrier::{timestamp::Timestamp as InnerTimestamp, datetime::DateTime as InnerDateTime};
+use calendrier_with_offset::{timestamp::Timestamp as InnerTimestampWithOffset, datetime::DateTime as InnerDateTimeWithOffset};
 
 mod timestamp;
 mod datetime;
