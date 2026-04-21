9d858e884504a6bf83a912bdd0c7b6af4eb24e9d
Mubelotix <mubelotix@dera.page>
2026-04-21 02:12:29 +0200

    Add new function

--- wrapper/src/lib.rs
+++ wrapper/src/lib.rs
@@ -6,3 +6,9 @@ mod datetime;
 
 pub use timestamp::*;
 pub use datetime::*;
+
+#[cfg(feature = "solar")]
+#[wasm_bindgen]
+pub fn get_solar_time_speed_ratio(unix_timestamp: i64, window: usize) -> f64 {
+	calendrier::solar::get_solar_time_speed_ratio(unix_timestamp, window)
+}
