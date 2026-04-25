44226ad681dc9dde91c70a62cbcaa70080c4cd5d
Mubelotix <mubelotix@dera.page>
2026-04-25 09:51:31 +0200

    Remove import map

--- wrapper/build.sh
+++ wrapper/build.sh
@@ -22,3 +22,8 @@ ln -sfn ../wrapper/pkg/calendrier_web.js ../static/calendrier_web.js
 ln -sfn ../wrapper/pkg/calendrier_web_bg.wasm ../static/calendrier_web_bg.wasm
 ln -sfn ../wrapper/pkg-solar/calendrier_web.js ../static/calendrier_web_solar.js
 ln -sfn ../wrapper/pkg-solar/calendrier_web_bg.wasm ../static/calendrier_web_solar_bg.wasm
+
+# calendrier >= 1.1 does not require a custom env shim, strip stale glue imports if emitted.
+sed -i 's|^import \* as import1 from "env"$|const import1 = {};|' pkg-solar/calendrier_web.js
+sed -i 's|^import \* as import2 from "env"$|const import2 = {};|' pkg-solar/calendrier_web.js
+sed -i 's|^        "env": import2,$|        "env2": import2,|' pkg-solar/calendrier_web.js
